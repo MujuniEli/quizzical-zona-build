74e6e77f08bb694db5742381039a77027d162ac7
MujuniEli <elijahmujuni@gmail.com>
2023-02-23 08:13:19 +0300

    add intro page

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
+import blobBlu from './assets/blob blue.svg'
+import blobYello from './assets/blob yellow.svg'
+import Intro from './components/intro'
 
 
 
-function App() {
+
+export default function App() {
   
 
   return (
     <div className="App">
-      
+      <img src={blobYello} alt="yellow blob" className='blob-yellow' />
+      <Intro />
+      <img src={blobBlu} alt="blue blob" className='blob-blue' />
     </div>
   )
 }
 
-export default App
+
---
+++ src/components/intro.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+export default function Intro() {
+    return (
+        <div className="intro-container">
+            <h1>Quizzical</h1>
+             <p className="description">Welcome to quizzical the App that lets you test your knowledge press start to get started!! </p>
+             <button type='button' className='start-btn'>Start Quiz</button>
+        </div>
+    )
+}
