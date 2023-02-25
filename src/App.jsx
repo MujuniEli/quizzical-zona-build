1f0383ce8d65a8deb4ca0420b77a04c88b9e8070
MujuniEli <elijahmujuni@gmail.com>
2023-02-25 16:33:11 +0300

    update question fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'
+import { nanoid } from 'nanoid'
 import blobBlu from './assets/blob blue.svg'
 import blobYello from './assets/blob yellow.svg'
 import Intro from './components/intro'
@@ -12,6 +13,20 @@ export default function App() {
   const [question, setQuestion] = useState([])
 
   const API_URL = 'https://opentdb.com/api.php?amount=5&category=9&difficulty=medium&type=multiple';
+
+  useEffect(() => {
+    fetch(API_URL)
+    .then(response => response.json())
+    .then(data => setQuestion(data.results.map(item => {
+      const { question, correct_answer, incorrect_answer } = item
+      return {
+        id: nanoid(),
+        question: question,
+        correct_answer: correct_answer,
+        answers: [...incorrect_answer, correct_answer]
+      }
+    })))
+  })
 
   return (
     <div className="App">
