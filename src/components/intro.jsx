ac395fde45581d4b477d0d48886a3b4cd035823a
MujuniEli <elijahmujuni@gmail.com>
2023-03-14 07:50:57 +0300

    update start page mark up

--- src/components/intro.jsx
+++ src/components/intro.jsx
@@ -5,7 +5,7 @@ export default function Intro({ displayQuestions }) {
     return (
         <div className="intro-container">
             <h1>Quizzical</h1>
-             <h4 className="description">Welcome to quizzical the App that lets you test your knowledge, press start to get started!! </h4>
+             <h4 className="description">Welcome to quizzical the App that lets you test your knowledge, click! <b>start-quiz</b> button to get started!! </h4>
              <button type='button' className='start-btn' onClick={displayQuestions}>Start quiz</button>
         </div>
     )
